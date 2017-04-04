daebc740870ffb9b271b474f0d1408068c5ceb19
Stagiaire HPI <hpigithub@gmail.com>
2017-04-04 14:33:45 +0200

    ProfileAvatar
    How to Basics React

--- app/index.js
+++ app/index.js
@@ -1,15 +1,31 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Helloworld = React.createClass({
+var DataProfile = {
+  name : "Kevin",
+  imageUrl : "http://www.rd.com/wp-content/uploads/sites/2/2016/04/01-cat-wants-to-tell-you-laptop.jpg"
+}
+var ProfilPic = React.createClass({
   render: function() {
-    return(
-      <div> Hello World </div>
-    )
+    return <img src={this.props.imageUrl} style={{height : 100, width :100}}/>
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function() {
+    return <div> {this.props.name} </div>
+  }
+});
+
+var AvatarContainer = React.createClass({
+  render: function() {
+    return  <div>
+      <ProfileName name={this.props.profile.name}/>
+      <ProfilPic imageUrl={this.props.profile.imageUrl}/> </div>
   }
 });
 
 ReactDOM.render(
-  <Helloworld/>,
+  <AvatarContainer profile={DataProfile}/>,
   document.getElementById('app')
 );
