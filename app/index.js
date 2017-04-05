ff0bcd50c72296aac101d5819d2140d7826851b7
Stagiaire HPI <hpigithub@gmail.com>
2017-04-05 10:05:43 +0200

    Routes
    Not working, location undifined

--- app/index.js
+++ app/index.js
@@ -1,31 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var DataProfile = {
-  name : "Kevin",
-  imageUrl : "http://www.rd.com/wp-content/uploads/sites/2/2016/04/01-cat-wants-to-tell-you-laptop.jpg"
-}
-var ProfilPic = React.createClass({
-  render: function() {
-    return <img src={this.props.imageUrl} style={{height : 100, width :100}}/>
-  }
-});
+var routes = require('./config/routes');
 
-var ProfileName = React.createClass({
-  render: function() {
-    return <div> {this.props.name} </div>
-  }
-});
 
-var AvatarContainer = React.createClass({
-  render: function() {
-    return  <div>
-      <ProfileName name={this.props.profile.name}/>
-      <ProfilPic imageUrl={this.props.profile.imageUrl}/> </div>
-  }
-});
-
-ReactDOM.render(
-  <AvatarContainer profile={DataProfile}/>,
-  document.getElementById('app')
-);
+ReactDOM.render(routes, document.getElementById('app'));
